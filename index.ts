a04746408d439f97a99b1c4f5546399a92101597
Todd Adelgren <toddadelgren@iphone.mlc.edu>
2020-02-07 16:53:45 -0600

    File Added

---
+++ index.ts
@@ -0,0 +1,6 @@
+document.getElementById('sign-in').addEventListener('click', function(e){
+    e.preventDefault();
+    let emailAddress: string = document.getElementById('email-address').value;
+})
+
+document.getElementById('email-address').focus();

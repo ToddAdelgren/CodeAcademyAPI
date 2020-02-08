1523c2e5661f5d36a39cfcc56951a0624a142cb5
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-02-08 17:31:34 -0600

    Finished Field Validation

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 document.getElementById('sign-in').addEventListener('click', function (e) {
     e.preventDefault();
-    var emailAddress = document.getElementById('email-address').value;
-    console.log(emailAddress);
+    var emailAddress = document.getElementById('email-address').value.trim();
+    if (fieldIsValid(emailAddress, 'email-address')) {
+        var password = document.getElementById('password').value.trim();
+        if (fieldIsValid(password, 'password')) {
+            console.log('THE FIELDS ARE VALID');
+        }
+    }
 });
 document.getElementById('email-address').focus();
+function fieldIsValid(enteredValue, elementId) {
+    if (enteredValue.length === 0) {
+        document.getElementById(elementId).classList.add('is-invalid');
+        document.getElementById(elementId).focus();
+        return false;
+    }
+    else {
+        document.getElementById(elementId).classList.remove('is-invalid');
+        return true;
+    }
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,33 @@
 document.getElementById('sign-in').addEventListener('click', function(e){
+
     e.preventDefault();
-    let emailAddress: string = (<HTMLInputElement>document.getElementById('email-address')).value;
-    console.log(emailAddress);
+
+    let emailAddress: string = (<HTMLInputElement>document.getElementById('email-address')).value.trim();
+
+    if (fieldIsValid(emailAddress, 'email-address')){
+
+        let password: string = (<HTMLInputElement>document.getElementById('password')).value.trim();
+
+        if (fieldIsValid(password, 'password')){
+
+            console.log('THE FIELDS ARE VALID');
+        }
+    }
 })
 
 document.getElementById('email-address').focus();
+
+function fieldIsValid(enteredValue: string, elementId: string): boolean {
+
+    if (enteredValue.length === 0){
+
+        document.getElementById(elementId).classList.add('is-invalid');
+        document.getElementById(elementId).focus();
+        return false;
+
+    } else {
+
+        document.getElementById(elementId).classList.remove('is-invalid');
+        return true;
+    }
+}

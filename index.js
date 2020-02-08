6fb95c0c355d84a411fd8653518b2cc4046bc68a
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-02-08 09:04:29 -0600

    Working Sign In Button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.getElementById('sign-in').addEventListener('click', function (e) {
     e.preventDefault();
     var emailAddress = document.getElementById('email-address').value;
+    console.log(emailAddress);
 });
 document.getElementById('email-address').focus();
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 document.getElementById('sign-in').addEventListener('click', function(e){
     e.preventDefault();
     let emailAddress: string = (<HTMLInputElement>document.getElementById('email-address')).value;
+    console.log(emailAddress);
 })
 
 document.getElementById('email-address').focus();
